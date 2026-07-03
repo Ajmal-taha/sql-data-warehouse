4065d77c2780fccb43defc3b0e76c2f7ff0d17ba
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2026-07-03 23:13:32 +0530

    Create bronze layer tables in the data warehouse
    This script creates tables in the bronze layer of the data warehouse. It drops existing tables if they exist before creating new ones.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,89 @@
+/*
+==============================================================
+CREATING THE BRONZE LAYER TABLES
+==============================================================
+This script is used to create the tables in the bronze layer.
+if the tables already exists they are dropped first then created again.
+tables created:
+	- bronze.crm_cust_info
+	- bronze.crm_prod_info
+	- bronze.crm_sales_details
+	- bronze.erp_loc_a101
+	- bronze.erp_cust_az12
+	- bronze.erp_px_cat_g1v2
+WARNING: this script DROPS the tables if it already exists.
+		make sure to keep the backup of original table if necessary.
+*/
+
+
+USE Datawarehouse;
+GO
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info (
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE
+);
+GO
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id			INT,
+	prd_key			NVARCHAR(50),
+	prd_nm			NVARCHAR(100),
+	prd_cost		INT,
+	prd_line		NVARCHAR(50),
+	prd_start_dt	DATETIME,
+	prd_end_dt		DATETIME
+);
+GO
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num		NVARCHAR(50),
+	sls_prd_key		NVARCHAR(50),
+	sls_cust_id		INT,
+	sls_order_dt	INT,
+	sls_ship_dt		INT,
+	sls_due_dt		INT,
+	sls_sales		INT,
+	sls_quantity	INT,
+	sls_price		INT
+);
+GO
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	cid		NVARCHAR(50),
+	cntry	NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+	cid		NVARCHAR(50),
+	bdate	DATE,
+	gen		NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
+	subcat		NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+GO
